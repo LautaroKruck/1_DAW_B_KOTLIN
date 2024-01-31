8bcbe2288ca5e9af64b603905d831b24dff31ad4
lkru144 <lkru144@g.educaand.es>
2024-01-31 17:42:05 +0100

    entornos

---
+++ juego_escapada/src/test/kotlin/HabitacionTest.kt
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class HabitacionTest {
+
+    @Test
+    fun moverIzquierda() {
+        val habitacion = Habitacion(10, 10)
+
+        assertEquals('1', habitacion.miraIzquierda())
+    }
+
+    @Test
+    fun moverDerecha() {
+        val habitacion = Habitacion(10,10)
+
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        assertEquals('P', habitacion.miraDerecha())
+    }
+
+    @Test
+    fun moverArriba() {
+        val habitacion = Habitacion(10, 10)
+        assertEquals('1', habitacion.miraArriba())
+    }
+
+    @Test
+    fun moverAbajo() {
+        val habitacion = Habitacion(10, 10)
+        habitacion.moverAbajo()
+        habitacion.moverAbajo()
+        habitacion.moverAbajo()
+        habitacion.moverAbajo()
+        habitacion.moverAbajo()
+        habitacion.moverAbajo()
+
+        assertEquals('X', habitacion.miraAbajo())
+    }
+
+    @Test
+    fun miraArriba() {
+        val habitacion = Habitacion(10, 10)
+        habitacion.moverAbajo()
+        habitacion.moverAbajo()
+        habitacion.moverDerecha()
+        assertEquals('X', habitacion.miraArriba())
+    }
+
+    @Test
+    fun miraAbajo() {
+        val habitacion = Habitacion(10, 10)
+        habitacion.moverDerecha()
+        assertEquals('X', habitacion.miraAbajo())
+    }
+
+    @Test
+    fun miraIzquierda() {
+        val habitacion = Habitacion(10, 10)
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverDerecha()
+        habitacion.moverAbajo()
+        assertEquals('X', habitacion.miraIzquierda())
+    }
+
+    @Test
+    fun miraDerecha() {
+        val habitacion = Habitacion(10, 10)
+        habitacion.moverAbajo()
+        assertEquals('X', habitacion.miraDerecha())
+    }
+}
