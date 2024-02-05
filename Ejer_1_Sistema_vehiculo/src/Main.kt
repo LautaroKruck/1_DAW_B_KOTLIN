66f2f26f08c0ad225c1ce11e660867306a46c978
lkru144 <lkru144@g.educaand.es>
2024-02-05 15:44:03 +0100

    ejer 2

--- Ejer_1_Sistema_vehiculo/src/Main.kt
+++ Ejer_1_Sistema_vehiculo/src/Main.kt
@@ -1,10 +1,10 @@
 fun main() {
     val micoche = Automovil("Ferrari", "deportivo", 1000)
     println(micoche)
-    micoche.calcularAutonomia()
+    println(micoche.calcularAutonomia())
     val miauto = Automovil("Ford", "familiar", 750)
     println(miauto)
-    miauto.calcularAutonomia()
+    println(miauto.calcularAutonomia())
     val mimoto = Motocicleta("Honda", "deportiva", 1000, 1000)
     println(mimoto)
     println(mimoto.calcularAutonomia())
--- Ejer_2_GestionAcademica/src/Estudiante2.kt
+++ Ejer_2_GestionAcademica/src/Estudiante2.kt
@@ -1,10 +1,10 @@
-@Suppress("UNREACHABLE_CODE")
-class Estudiante2(private var nombre: String, private var edad: Int, private var curso: String, private var calificacion: Double): Persona2(nombre, edad) {
+
+class Estudiante2(var nombre: String, var edad: Int, private var curso: String, private var calificacion: Double): Persona2(nombre, edad) {
     override fun toString(): String {
         return "Nombre: $nombre, Edad: $edad, Id: $id"
         return super.toString() + ",Curso: $curso, Calificacion promedio: $calificacion"
     }
-    override fun mostrarRol() {
+    override fun mostrarRol(): String {
         return "Soy un Estudiante"
     }
 }
