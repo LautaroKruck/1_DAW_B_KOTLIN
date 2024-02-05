08843194f1d7756776539d3a49c2a059be495512
lkru144 <lkru144@g.educaand.es>
2024-02-05 16:47:24 +0100

    ultimos ejers ordendos

--- Ejer_3_Persona_Estudiante/src/Estudiante.kt
+++
@@ -1,8 +0,0 @@
-class Estudiante(nombre: String, edad: Int, val carrera: String) : Persona(nombre, edad ) {
-    override fun toString(): String {
-        return super.toString().dropLast(2).replace("Persona", "Estudiante") + ", carrera = $carrera )"
-    }
-    override fun actividad(): String {
-        return "$nombre está estudiando para un examen"
-    }
-}
