09a0e22a0d13a9c3fbedc2db2c1b5a82664b6178
lkru144 <lkru144@g.educaand.es>
2024-02-06 20:32:28 +0100

    ejer 5 repo

--- Ejer_5_Articulos/src/Ordenador.kt
+++
@@ -1,8 +0,0 @@
-class Ordenador (nombre: String, precio: Double, val tipoOrdenador: TipoOrdenador = TipoOrdenador.BASICO): Articulo(nombre, precio) {
-
-    fun promocionNavidad(DescuentoNavidad: Double = 30.0) {
-        if (precio > 500.00) {
-            precio -= (precio * DescuentoNavidad/100)
-        }
-    }
-}
--- Ejer_5_Articulos/src/TipoOrdenador.kt
+++
@@ -1,3 +0,0 @@
-enum class TipoOrdenador {
-    BASICO, OFIMATICA, TODOTERRENO, GAMING
-}
